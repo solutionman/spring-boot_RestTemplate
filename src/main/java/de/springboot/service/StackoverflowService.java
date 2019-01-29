aebdd2731c5e402f90db852bf29cedae56dfe521
lom <solutionman@inbox.ru>
2019-01-29 21:25:50 +0700

    all works

--- src/main/java/de/springboot/service/StackoverflowService.java
+++ src/main/java/de/springboot/service/StackoverflowService.java
@@ -9,6 +9,10 @@ import java.util.List;
 @Service
 public class StackoverflowService {
     private static List<StackoverflowWebsite> items = new ArrayList<>();
+    static {
+        items.add(new StackoverflowWebsite("stackoverflow", "http://stackoverflow.com","http://cdn.sstatic.net/Sites/stackoverflow/img/favicon.ico", "Stack Overflow (StackExchange)", "for professional and enthusiast programmers"));
+        items.add(new StackoverflowWebsite("serverfault", "http://serverfault.com","http://cdn.sstatic.net/Sites/serverfault/img/favicon.ico", "Server Fault (StackExchange)", "for system and network administrators"));
+    }
 
     public List<StackoverflowWebsite> findAll(){
         return items;
--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,11 +1,9 @@
 var app = angular.module("springDemo",[]);
 
-app.controller("AppCtrl",function ($scope) {
-    $scope.websites = [{
-        iconImageUrl: '',
-        id: 'stackoverflow',
-        website: 'stackoverflow.com',
-        title: 'Stackoverflow website',
-        description: 'Stackoverflow description'
-    }];
+app.controller("AppCtrl",function($scope, $http) {
+    $scope.websites = [];
+
+    $http.get('http://localhost:8099/api/stackoverflow').then(function(response){
+        $scope.websites = response.data;
+    });
 });
