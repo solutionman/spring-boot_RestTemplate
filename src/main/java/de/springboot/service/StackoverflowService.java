13875cdef7446805c6936e8a6effa5ec1526acf3
lom <solutionman@inbox.ru>
2019-01-29 20:19:27 +0700

    no errors 2

--- src/main/java/de/springboot/service/StackoverflowService.java
+++ src/main/java/de/springboot/service/StackoverflowService.java
@@ -3,12 +3,14 @@ package de.springboot.service;
 import de.springboot.model.StackoverflowWebsite;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class StackoverflowService {
+    private static List<StackoverflowWebsite> items = new ArrayList<>();
 
     public List<StackoverflowWebsite> findAll(){
-        return null;
+        return items;
     }
 }
