2db9328a37457d2ae35b429f0268ee9ca699fca1
lom <solutionman@inbox.ru>
2019-02-02 10:12:16 +0700

    test works

--- src/test/java/de/springboot/web/StackoverflowControllerTest.java
+++ src/test/java/de/springboot/web/StackoverflowControllerTest.java
@@ -1,17 +1,27 @@
 package de.springboot.web;
 
+import de.springboot.model.StackoverflowWebsite;
+import de.springboot.service.StackoverflowService;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class StackoverflowControllerTest {
-
-    @Before
-    public void setUp() throws Exception {
-    }
+    @Mock
+    private StackoverflowService stackoverflowService;
+    @InjectMocks
+    StackoverflowController sut;
 
     @Test
     public void getListOfProviders() {
+        List<StackoverflowWebsite> listOfProviders = sut.getListOfProviders();
     }
 }
