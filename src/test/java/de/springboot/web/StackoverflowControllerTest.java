b48f2b313b39492b3909202d770d3b32a8d0e61d
lom <solutionman@inbox.ru>
2019-02-02 09:10:13 +0700

    tests beging

---
+++ src/test/java/de/springboot/web/StackoverflowControllerTest.java
@@ -0,0 +1,17 @@
+package de.springboot.web;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StackoverflowControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getListOfProviders() {
+    }
+}
