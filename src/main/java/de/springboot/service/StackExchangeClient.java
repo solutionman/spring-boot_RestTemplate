a3011662fd6bd4dae11a6fa828179075df3b5fb6
lom <solutionman@inbox.ru>
2019-02-03 15:38:12 +0700

    works with gzip

--- src/main/java/de/springboot/service/StackExchangeClient.java
+++ src/main/java/de/springboot/service/StackExchangeClient.java
@@ -3,6 +3,10 @@ package de.springboot.service;
 import de.springboot.model.SiteDto;
 import de.springboot.model.SitesDto;
 import de.springboot.model.StackoverflowWebsite;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +16,9 @@ import java.util.List;
 
 @Component
 public class StackExchangeClient {
-    private RestTemplate restTemplate = new RestTemplate();
+    HttpClient httpClient = HttpClientBuilder.create().build();
+    ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+    private RestTemplate restTemplate = new RestTemplate(requestFactory);
 
     public List<SiteDto> getSites() {
         String url = "https://api.stackexchange.com/2.2/sites?page=1&pagesize=9999&filter=!)QmCwY51ivjfd_pA.icNAOWD";
